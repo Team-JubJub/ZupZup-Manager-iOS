69a197e4fd2be3d04fb3b61b3e80ceecd85d31ea
luminoux <yungzin98@naver.com>
2023-02-18 22:40:03 +0900

    :sparkles: App Start Point 설정

---
+++ Sources/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  pinto
+//
+//  Created by mctdev on 2023/01/02.
+//  Copyright © 2023 machete. All rights reserved.
+//
+
+import SwiftUI
+
+// MARK: AppDelegate
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
---
+++ Sources/App/ZupZupApp.swift
@@ -0,0 +1,14 @@
+import Foundation
+import SwiftUI
+
+@main
+struct ZupZupApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
