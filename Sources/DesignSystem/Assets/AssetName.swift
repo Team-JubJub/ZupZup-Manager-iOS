02ef74474b7dd22bd5da064f1edb9e9c1ad2c063
luminoux <yungzin98@naver.com>
2023-02-20 12:24:16 +0900

    :sparkles: AssetName 추기

---
+++ Sources/DesignSystem/Assets/AssetName.swift
@@ -0,0 +1,13 @@
+//
+//  AssetName.swift
+//  ZupZupManager
+//
+//  Created by YeongJin Jeong on 2023/02/20.
+//  Copyright © 2023 ZupZup. All rights reserved.
+//
+
+import Foundation
+
+enum AssetName: String {
+    
+}
