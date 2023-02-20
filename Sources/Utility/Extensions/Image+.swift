10eb487d54c639c84c753f5d5b0acb271394d4e3
YeongJin Jeong <81943525+luminouxx@users.noreply.github.com>
2023-02-20 13:49:51 +0900

    :sparkles: 이미지 상수화 진행

---
+++ Sources/Utility/Extensions/Image+.swift
@@ -0,0 +1,15 @@
+//
+//  Image+.swift
+//  ZupZupManager
+//
+//  Created by YeongJin Jeong on 2023/02/20.
+//  Copyright © 2023 ZupZup. All rights reserved.
+//
+
+import SwiftUI
+
+extension Image {
+    init(assetName: AssetName) {
+        self.init(assetName.rawValue)
+    }
+}
