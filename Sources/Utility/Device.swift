e52156a8eef129be71446b4a8e81f580fdf4590f
luminoux <yungzin98@naver.com>
2023-02-22 19:39:34 +0900

    :recycle: Vertical -> V로 네이밍 변경

--- Sources/Utility/Device.swift
+++ Sources/Utility/Device.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Device {
     static let Width = UIScreen.main.bounds.width
     static let Height = UIScreen.main.bounds.height
-    static let VerticalPadding = UIScreen.main.bounds.width * 16 / 390
-    static let horizontalPadding = UIScreen.main.bounds.height * 16 / 844
+    static let VPadding = UIScreen.main.bounds.width * 16 / 390
+    static let HPadding = UIScreen.main.bounds.height * 16 / 844
     static let cornerRadious = UIScreen.main.bounds.width * 14 / 390
 }
