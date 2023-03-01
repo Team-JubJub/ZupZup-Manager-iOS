ddb71caef959aa9fbe2ce4386dfabe1d88c19dd7
luminoux <yungzin98@naver.com>
2023-03-01 17:52:17 +0900

    :sparkles: LoginView: 컴포넌트 분리

--- Sources/Views/LoginView.swift
+++ Sources/Views/LoginView.swift
@@ -10,49 +10,25 @@ import SwiftUI
 
 struct LoginView: View {
     
-    @State var idString: String = "아이디"
-    @State var password: String = "비밀번호"
+    @State var idString: String = ""
+    @State var password: String = ""
     
     var body: some View {
-        VStack {
-            
+        VStack(spacing: 0) {
+            Spacer()
+            BoxImage()
             Spacer()
             
-            ZStack {
-                Circle()
-                    .frame(width: 200, height: 200)
-                Image(assetName: .box)
-                    .resizable()
-                    .frame(width: 96, height: 113)
-                    .scaledToFit()
-            }
-            
+            IdTextField(idString: $idString)
+            VSpacer(height: 20)
+            PasswordTextField(password: $password)
             Spacer()
-            TextField(text: $idString, prompt: "아") {
-                Rectangle()                    
-            }
             
             Button {
                 print("button tabbed")
             } label: {
-                ZStack {
-                    Rectangle()
-                        .cornerRadius(14)
-                        .foregroundColor(.designSystem(.zupzupMain))
-                        .frame(height: Device.Height * 57 / 844)
-                        .padding(EdgeInsets(top: 0, leading: Device.horizontalPadding, bottom: 0, trailing: Device.horizontalPadding))
-                    Text("로그인")
-                        .font(SystemFont(size: ._17, weight: .bold))
-                        .foregroundColor(.designSystem(.Secondary))
-                }
+                LoginBottomButton()
             }
-
         }
-    }
-}
-
-struct LoginView_Previews: PreviewProvider {
-    static var previews: some View {
-        LoginView()
     }
 }
