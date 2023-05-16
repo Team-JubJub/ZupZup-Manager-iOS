7c37a92294354cab6927370284814f3900bcb843
luminoux <yungzin98@naver.com>
2023-05-16 16:22:24 +0900

    :sparkles: FetchStoreUseCase, Repo 추가

---
+++ Sources/Domain/UseCase/FetchStoreUseCase.swift
@@ -0,0 +1,38 @@
+//
+//  FetchStoreUseCase.swift
+//  ZupZupManager
+//
+//  Created by YeongJin Jeong on 2023/05/15.
+//  Copyright © 2023 ZupZup. All rights reserved.
+//
+
+import Foundation
+
+protocol FetchStoreUseCase {
+    func fetchStore(storeId: Int, completion: @escaping (Result<Store, Error>) -> Void)
+}
+
+final class FetchStoreUseCaseImpl: FetchStoreUseCase {
+        
+    private let fetchStoreRepository: FetchStoreRepository
+    
+    init(fetchStoreRepository: FetchStoreRepository = FetchStoreRepositoryImpl()) {
+        self.fetchStoreRepository = fetchStoreRepository
+    }
+    
+    func fetchStore(
+        storeId: Int,
+        completion: @escaping (Result<Store, Error>) -> Void
+    ) {
+        print("useCase")
+        self.fetchStoreRepository.fetchStore(storeId: storeId) { result in
+            switch result {
+            case .success(let storeDTO):
+                let store = storeDTO.toStore()
+                completion(.success(store))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
