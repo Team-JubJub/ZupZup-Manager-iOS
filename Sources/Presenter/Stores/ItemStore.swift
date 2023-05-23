3cc0dce48161115928512be84f3684e7e3caeae5
luminoux <yungzin98@naver.com>
2023-05-23 15:11:39 +0900

    :sparkles: ItemStore : Action -> State 정의

--- Sources/Presenter/Stores/ItemStore.swift
+++ Sources/Presenter/Stores/ItemStore.swift
@@ -11,8 +11,50 @@ import SwiftUI
 class ItemStore: ObservableObject {
     
     @Published var item: Item
+    @Published var isShowingImagePicker: Bool = false
+    @Published var selectedImage: UIImage?
     
     init(item: Item) {
         self.item = item
+    }
+}
+
+extension ItemStore: StoreProtocol {
+    enum Action {
+        case tabImagePickerButton
+        case tabTrashTong
+        case tabMinusButton
+        case tabPlusButton
+    }
+    
+    func reduce(action: Action) {
+        switch action {
+        case .tabImagePickerButton:
+            self.tabImagePickerButton()
+        case .tabTrashTong:
+            self.tabTrashTong()
+        case .tabMinusButton:
+            self.tabMinusButton()
+        case .tabPlusButton:
+            self.tabPlusButton()
+        }
+    }
+}
+
+extension ItemStore {
+    func tabImagePickerButton() {
+        self.isShowingImagePicker = true
+    }
+    
+    func tabTrashTong() {
+        
+    }
+    
+    func tabMinusButton() {
+        
+    }
+    
+    func tabPlusButton() {
+        
     }
 }
