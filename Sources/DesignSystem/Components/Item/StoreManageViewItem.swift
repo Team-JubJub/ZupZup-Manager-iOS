bac41e6c18a26cc02208f073e8bb65d1ce4bddc7
luminoux <yungzin98@naver.com>
2023-07-05 14:55:09 +0900

    :lipstick: RightChevronButton & StoreManageViewItem 추가

---
+++ Sources/DesignSystem/Components/Item/StoreManageViewItem.swift
@@ -0,0 +1,28 @@
+//
+//  StoreManageViewItem.swift
+//  ZupZupManager
+//
+//  Created by YeongJin Jeong on 2023/07/04.
+//  Copyright © 2023 ZupZup. All rights reserved.
+//
+
+import SwiftUI
+
+struct StoreManageViewItem: View {
+    
+    let title: String
+    let action: () -> Void
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            SystemLabel(text: title, typo: .callout)
+            InfiniteSpacer()
+            RightChevronButton { action() }
+        }
+        .frame(width: Device.Width * 350 / 390, height: 60)
+        
+        Rectangle()
+            .frame(height: 1)
+            .foregroundColor(.designSystem(.zupzupIvoryGray200))
+    }
+}
