8f61b735fcd69627339c0e3f155458b728968872
luminouxx <yungzin98@naver.com>
2023-07-06 13:24:53 +0900

    :recycle: NavigationBar Bug 해결

--- Sources/Presenter/Views/ReservationView.swift
+++ Sources/Presenter/Views/ReservationView.swift
@@ -13,34 +13,37 @@ struct ReservationView: View {
     @StateObject var reservationStore: ReservationStore
     
     var body: some View {
-        ScrollView(showsIndicators: false) {
-            if reservationStore.isLoading {
-                ProgressView()
-                    .progressViewStyle(.circular)
-                    .padding()
-            } else {
-                VSpacer(height: Device.VPadding)
-                VStack(spacing: 8) {
-                    ForEach(reservationStore.reservations, id: \.self) { reservation in
-                        NavigationLink {
-                            let store = ReservationDetailStore(
-                                reservation: reservation,
-                                store: reservationStore.store
-                            )
-                            ReserveDetailView(store: store)
-                        } label: {
-                            ReservationItem(
-                                date: reservation.date,
-                                menu: reservation.orderedItemdName,
-                                time: reservation.orderedTime,
-                                customer: reservation.customerName,
-                                state: reservation.state
-                            )
-                        }
+        if reservationStore.isLoading {
+            ProgressView()
+                .progressViewStyle(.circular)
+                .padding()
+        } else {
+            VSpacer(height: Device.VPadding)
+            VStack(spacing: 8) {
+                HStack(spacing: 0) {
+                    SuiteLabel(text: "예약 상황", typo: .hero, color: .designSystem(.Secondary))
+                    InfiniteSpacer()
+                }
+                .padding(EdgeInsets(top: Device.Height * 46 / 844, leading: Device.HPadding, bottom: Device.Height * 20 / 844, trailing: Device.HPadding))
+                
+                ForEach(reservationStore.reservations, id: \.self) { reservation in
+                    NavigationLink {
+                        let store = ReservationDetailStore(
+                            reservation: reservation,
+                            store: reservationStore.store
+                        )
+                        ReserveDetailView(store: store)
+                    } label: {
+                        ReservationItem(
+                            date: reservation.date,
+                            menu: reservation.orderedItemdName,
+                            time: reservation.orderedTime,
+                            customer: reservation.customerName,
+                            state: reservation.state
+                        )
                     }
                 }
-            }
+            }.navigationTitle("")
         }
-        .navigationTitle("예약 상황")
     }
 }
