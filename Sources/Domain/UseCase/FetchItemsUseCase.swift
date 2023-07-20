2328dac32f93b46178d5c330f7c9e2842b7ac14a
luminouxx <yungzin98@naver.com>
2023-07-20 20:38:23 +0900

    :sparkles: FetchItems 유즈케이스 추가

---
+++ Sources/Domain/UseCase/FetchItemsUseCase.swift
@@ -0,0 +1,34 @@
+//
+//  FetchItemsUseCase.swift
+//  ZupZupManager
+//
+//  Created by YeongJin Jeong on 2023/07/20.
+//  Copyright © 2023 ZupZup. All rights reserved.
+//
+
+import Foundation
+
+protocol FetchItemsUseCase {
+    func fetchItems(completion: @escaping (Result<[ItemEntity], NetworkError>) -> Void)
+    
+}
+
+final class FetchItemsUseCaseImpl: FetchItemsUseCase {
+    
+    let fetchItemsRepository: FetchItemsRepository
+    
+    init(fetchItemsRepository: FetchItemsRepository = FetchItemsRepositoryImpl()) {
+        self.fetchItemsRepository = fetchItemsRepository
+    }
+    
+    func fetchItems(completion: @escaping (Result<[ItemEntity], NetworkError>) -> Void) {
+        self.fetchItemsRepository.fetchItems { result in
+            switch result {
+            case .success(let response):
+                completion(.success( response.map { $0.toItem() }))
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
