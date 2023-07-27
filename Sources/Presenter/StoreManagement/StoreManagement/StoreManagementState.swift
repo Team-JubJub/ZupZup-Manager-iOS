57a1426a436feef07b148bda1ed49bb6108bc521
luminouxx <yungzin98@naver.com>
2023-07-27 23:38:19 +0900

    :memo: 주석 추가

--- Sources/Presenter/StoreManagement/StoreManagement/StoreManagementState.swift
+++ Sources/Presenter/StoreManagement/StoreManagement/StoreManagementState.swift
@@ -18,49 +18,49 @@ struct StoreManagementState: Equatable {
 enum StoreManagementAction: Equatable {
     case tapToggle // 가게 On / Off 토글
     case tapInfoButton // 가게 세부 정보 네비게이션
-    case isShowingEditStoreInfoBinding
-    case openStoreResponse(Result<OpenStoreResponse, NetworkError>)
-    case fetchStore
-    case fetchStoreResponse(Result<StoreEntity, NetworkError>)
+    case isShowingEditStoreInfoBinding // isShowingEditStoreInfo 변수 바인딩
+    case openStoreResponse(Result<OpenStoreResponse, NetworkError>) // 가게 ON/OFF API 호출의 결과
+    case fetchStore // 가게 정보 조희 API 호출
+    case fetchStoreResponse(Result<StoreEntity, NetworkError>) // 가게 정보 조희 API 호출의 결과
 }
 
 struct StoreManagementEnvironment {
-    let openStore: (OpenStoreRequest) -> EffectPublisher<Result<OpenStoreResponse, NetworkError>, Never>
-    let fetchStore: () -> EffectPublisher<Result<StoreEntity, NetworkError>, Never>
+    let openStore: (OpenStoreRequest) -> EffectPublisher<Result<OpenStoreResponse, NetworkError>, Never> // 가게 ON/OFF
+    let fetchStore: () -> EffectPublisher<Result<StoreEntity, NetworkError>, Never> // 가게 정보 조희
 }
 
 let storeManagementReducer = AnyReducer<StoreManagementState, StoreManagementAction, StoreManagementEnvironment> { state, action, environment in
     switch action {
-    case .tapToggle:
+    case .tapToggle: // 가게 On / Off 토글
         return environment.openStore(OpenStoreRequest(openOrClose: !state.storeEntity.isOpen))
             .map(StoreManagementAction.openStoreResponse)
             .eraseToEffect()
         
-    case .tapInfoButton:
+    case .tapInfoButton: // 가게 세부 정보 네비게이션
         state.isShowingEditStoreInfo = true
         return .none
         
-    case .isShowingEditStoreInfoBinding:
+    case .isShowingEditStoreInfoBinding: // isShowingEditStoreInfo 변수 바인딩
         state.isShowingEditStoreInfo = false
         return .none
         
-    case let .openStoreResponse(.success(response)):
+    case let .openStoreResponse(.success(response)): // 가게 ON/OFF API 호출의 결과 - 성공
         state.storeEntity.isOpen.toggle()
         return .none
         
-    case let .openStoreResponse(.failure(error)):
+    case let .openStoreResponse(.failure(error)): // 가게 ON/OFF API 호출의 결과 - 실패
         return .none
         
-    case .fetchStore:
+    case .fetchStore: // 가게 정보 조희 API 호출
         return environment.fetchStore()
             .map(StoreManagementAction.fetchStoreResponse)
             .eraseToEffect()
         
-    case let .fetchStoreResponse(.success(storeEntity)):
+    case let .fetchStoreResponse(.success(storeEntity)): // 가게 정보 조희 API 호출의 결과 - 성공
         state.storeEntity = storeEntity
         return .none
         
-    case let .fetchStoreResponse(.failure(error)):
+    case let .fetchStoreResponse(.failure(error)): // 가게 정보 조희 API 호출의 결과 - 실패
         // TODO: Error Handling
         return .none
     }
