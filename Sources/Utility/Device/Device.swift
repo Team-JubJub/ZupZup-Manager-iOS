45f18f326ee77c09ebed03fb01c2aeaa8bde4d94
luminouxx <yungzin98@naver.com>
2023-12-02 17:35:11 +0900

    :recycle: Debug, Release URL 분기

--- Sources/Utility/Device/Device.swift
+++ Sources/Utility/Device/Device.swift
@@ -9,11 +9,18 @@
 import SwiftUI
 
 struct Device {
+    
     static let Width = UIScreen.main.bounds.width
+    
     static let Height = UIScreen.main.bounds.height
+    
     static let WidthWithPadding = UIScreen.main.bounds.width * 358 / 390
+    
     static let HeightWithPadding = UIScreen.main.bounds.height * 812 / 844
+    
     static let VPadding = UIScreen.main.bounds.width * 16 / 390
+    
     static let HPadding = UIScreen.main.bounds.height * 16 / 844
+    
     static let cornerRadious = UIScreen.main.bounds.width * 14 / 390
 }
--- Sources/Utility/Manager/UrlManager.swift
+++ Sources/Utility/Manager/UrlManager.swift
@@ -8,33 +8,49 @@
 
 import UIKit
 
-// MARK: 웹으로 전환을 담당하는 클래스
 final class UrlManager {
-    // 싱글톤으로 관리
-    static let shared = UrlManager()
+    
+    static let shared = UrlManager()            // 싱글톤으로 관리
     
     private init() {}
+    
+#if DEBUG
+    static let baseUrl = "https://zupzuptest.com:8080"
+#else
+    static let baseUrl = "https://zupzupofficial.com:8080"
+#endif
+    
+#if DEBUG
+    static let findMyAccountUrl = "https://zupzuptest.com:3000/login"
+#else
+    static let findMyAccountUrl = "https://zupzupofficial.com/login"
+#endif
+    
+#if DEBUG
+    static let makeAccountUrl = "https://zupzuptest.com:3000/signin_terms"
+#else
+    static let makeAccountUrl = "https://zupzupofficial.com/signin_terms"
+#endif
+    static let customerCenterUrl = "https://www.notion.so/ZupZup-Help-9de26b33bf164a9594db321e63f6e7ce?pvs=4"
 }
 
 extension UrlManager {
     
     // MARK: 고객센터 웹으로 전환
     func openCustomerCenter() {
-        if let url = URL(string: "https://www.notion.so/ZupZup-Help-9de26b33bf164a9594db321e63f6e7ce?pvs=4") {
+        if let url = URL(string: UrlManager.customerCenterUrl) {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     func openFindMyAccount() {
-        // TODO: URL 수정
-        if let url = URL(string: "https://zupzuptest.com:3000/") {
+        if let url = URL(string: UrlManager.findMyAccountUrl) {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
     
     func openMakeAccount() {
-        // TODO: URL 수정
-        if let url = URL(string: "https://zupzuptest.com:3000/") {
+        if let url = URL(string: UrlManager.makeAccountUrl) {
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
